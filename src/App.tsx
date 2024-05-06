821d0c93cf07c3aed405de1bf997e94684d4c564
lmarch <lmarch@yandex.ru>
2024-05-07 00:21:42 +0300

    Adding Routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import Counter from "./components/Counter";
+import {Link, Route, Routes} from "react-router-dom";
 import './index.scss';
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 const App = () => {
     return (
         <div className="app">
-            Нажимай на кнопку!
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Routes>
+                <Route path={'/about'} element={<AboutPage />}/>
+                <Route path={'/'} element={<MainPage />}/>
+            </Routes>
         </div>
     )
 }
