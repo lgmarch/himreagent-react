9100932f0662c77d038a07b978d060c4d3b5f036
lmarch <lmarch@yandex.ru>
2024-05-07 01:14:16 +0300

    Adding Code-Splitting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import {Suspense} from "react";
 import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import './index.scss';
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 
 const App = () => {
     return (
         <div className="app">
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Routes>
-                <Route path={'/about'} element={<AboutPage />}/>
-                <Route path={'/'} element={<MainPage />}/>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     )
 }
