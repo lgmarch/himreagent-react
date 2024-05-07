b5f886b3b7d3b5dcaecc83fa95163515e28af887
lmarch <lmarch@yandex.ru>
2024-05-07 17:38:29 +0300

    Adding a light and dark theme to the project.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import {Suspense} from "react";
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import './index.scss';
+import "./styles/index.scss";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
